9c0b08e5fc5e504decc3c695bbb3b73787bc6e06
ashraful2871 <ashrafulislam2758@gmail.com>
2024-10-01 23:02:13 +0600

    murubbi

---
+++ test.js
@@ -0,0 +1,28 @@
+// let message = "Hello!";
+// setTimeout(() => {
+//   message = "Goodbye!";
+// }, 0);
+// console.log(message);
+// console.log("A");
+// Promise.resolve().then(() => console.log("B"));
+// setTimeout(() => console.log("C"), 0);
+// console.log("D");
+// try {
+//   console.log("Start");
+//   throw new Error("Error occurred");
+// } catch (err) {
+//   console.log(err.message);
+// } finally {
+//   console.log("End");
+// }
+
+// async function myFunction() {
+//   return "Hello";
+// }
+// myFunction().then(console.log);
+
+let x = 1;
+let intervalId = setInterval(() => {
+  console.log(x++);
+  if (x > 3) clearInterval(intervalId);
+}, 1000);
